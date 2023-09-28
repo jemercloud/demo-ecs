f74e40c09dd23c6e9950397943cd9ef9a2da9548
German Eichemberger <germaneichemberger@gmail.com>
2023-09-28 13:17:58 -0300

    fixes health

--- src/index.js
+++ src/index.js
@@ -14,6 +14,11 @@ app.get('*', (req, res) => {
   res.status(404).send('<h1>Page not found</h1>');
 });
 
+app.get('/management/health', (req, res) => {
+  logger.info(`${req.method} ${req.path}`);
+  res.send('ok');
+});
+
 app.listen(port, () => {
   logger.info(`process id: ${process.pid}`);
   logger.info(`server listening on port ${port}`);
